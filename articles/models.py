9a559ab76444abd7c966c6ac07c33ac78b8d5f1b
teja <tejaokay@gmail.com>
2020-10-14 09:17:55 +0530

    django_heroku updated

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Movie(models.Model):
+    id = models.BigIntegerField(primary_key = True)
     title=models.CharField(max_length=120,blank=False,null=False)
     content=models.TextField(null=False,blank=False)
     image=models.ImageField(null=False,blank=False)
@@ -10,17 +11,20 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 class Trailer(models.Model):
+    id = models.BigIntegerField(primary_key = True)
     title=models.CharField(max_length=120)
     links=models.TextField()
     def __str__(self):
         return self.title
 class Poster(models.Model):
+    id = models.BigIntegerField(primary_key = True)
     title=models.CharField(max_length=120)
     poster=models.ImageField()
     def __str__(self):
         return self.title
 
 class Cast(models.Model):
+    id = models.BigIntegerField(primary_key = True)
     name=models.CharField(max_length=120)
     photos=models.ImageField(blank=True)
     avatar=models.ImageField(blank=True)
